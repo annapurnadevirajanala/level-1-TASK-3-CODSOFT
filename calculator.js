83dc6d93d8105c7bb63f1b925f7dacad358faab1
annapurnadevirajanala <annapurnadevirajanala330@gmail.com>
2024-07-24 19:35:18 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,25 @@
+function appendNumber(number) {
+    const display = document.getElementById('display');
+    display.value += number;
+}
+
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+
+function deleteLast() {
+    const display = document.getElementById('display');
+    display.value = display.value.slice(0, -1);
+}
+
+
+function calculateResult() {
+    const display = document.getElementById('display');
+    try {
+        display.value = eval(display.value);
+    } catch {
+        display.value = 'Error';
+    }
+}
